01153e1c464aaab3266ba3d60a958881afbbe9f7
temp <temp@temps-MacBook-Pro-5.local>
2015-10-19 14:42:55 +0800

    add fibonacci.m

---
+++ fibonacci.m
@@ -0,0 +1,15 @@
+
+function f=fibonacci(n) %fibonacchi sequence function for n number
+
+
+f=zeroes(n,1); %sets first numbers of fibo seq
+
+f=0;
+f(1)=1;
+f(2)=2;
+
+for k=3:n;
+    f(k)=(k-1)+(k-2);
+ 
+end
+end
